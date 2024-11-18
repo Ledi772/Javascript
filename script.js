1ca9d220ab7f15505d94cd158667bc0530c140ff
Ledi772 <halilijuled2@gmail.com>
2024-11-18 09:44:32 -0800

    'test'

---
+++ script.js
@@ -0,0 +1,24 @@
+/*this is a lone Comment*/
+
+// variablat:
+// let emri = "Juled"
+// let notamatematika = 4;
+// let x;
+
+
+// deklarimi i variables= krijojme nje variabel
+let testVar;
+// inicializimi - variables i japin vlere per here te pare
+testVar ="m"
+
+// deklarimi + inicializimi ne te njejtin rresht
+const PI = 3.14
+
+// variablat & datatypea:
+let emri = "Juled"; //string
+let notaMatematike = 1; //number
+let isAdult = false; //true //boleane
+let x; //undefined
+let nullVariabla = null;
+
+//case sensitive 
