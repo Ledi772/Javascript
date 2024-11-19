b5d461425f61e6dadfec63e003d4293668421e13
Ledi772 <halilijuled2@gmail.com>
2024-11-19 09:46:17 -0800

    vazhdov APUNE NE FLDER TE NJEJTE

--- script.js
+++ script.js
@@ -22,3 +22,40 @@ let x; //undefined
 let nullVariabla = null;
 
 //case sensitive 
+let Emri = "Juled";
+
+ //alert( hello );
+//confirm("close window?")
+prompt("shkruaj emrin");
+
+alert("shfaq nje mesazh");
+
+let eshteMbi7 = confirm("A i ki mbi 7 vjet")
+console.log(eshteMbi7);
+
+let emri = prompt("A i ki mbi 7 vjet?");
+alert(" Hello " + emri);
+
+//pluss
+let nr1 = 4;
+let nr2 = 3;
+
+let shuma = nr1 + nr2; //7
+let ndryshimi = nr1 - nr2;
+let prodhimi = nr1 * nr2;
+let heresi = nr1/nr2;
+
+console.log("Shuma e" + nr1 + "dhe" + nr2 + "eshte" + shuma);
+console.log("Zbritja e" + nr1 + "dhe" + nr2 + "eshte" + ndryshimi);
+console.log("Shumezimi mes" + nr1 + "dhe" + nr2 + "eshte" + prodhimi);
+console.log("Hersi mes" + nr1 + "dhe" + nr2 + "eshte" + heresi);
+
+let ngritjaNeFuqi = 4 ** 2;
+
+let modulo = 16 & 3;
+
+let numri1 = +promt("shkruaj numrin e pare")
+let numri2 = +promt("shkruaj numrin e dyte")
+
+console.log(numri1 + numri2)
+//console.log(numri2)
